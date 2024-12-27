91b605aa807c35d64d6c8a79cf209cf6481b0261
gamwxristoi devs <fiercrusuer@gmail.com>
2024-12-27 16:45:21 +0200

    Add files via upload

---
+++ shell_zsh_fish_bash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ $(whoami) != "root" ]
+then
+   echo "sudo privilages required."
+exit 1
+fi
+
+echo -n "choose a shell. [ 1.bash./2.fish./3.zsh./ ]: "
+read num
+
+
+[ "$num" -eq "1" ] && sudo chsh "$USER" -s /bin/bash
+[ "$num" -eq "2" ] && sudo chsh "$USER" -s /bin/fish
+[ "$num" -eq "3" ] && sudo chsh "$USER" -S /bin/zsh
+
+[ -z "$num" ] && echo "invalid number."
